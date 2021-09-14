c04e201a43b56bf203861fe29958465bae3ab850
Peter <khriz8517@gmail.com>
2021-09-14 17:09:03 -0500

    intro elements

--- mod_form.php
+++ mod_form.php
@@ -25,6 +25,7 @@ class mod_revisionmaterial_mod_form extends moodleform_mod {
         $mform->setType('name', PARAM_TEXT);
         $mform->addRule('name', null, 'required', null, 'client');
 
+        $this->standard_intro_elements();
         $this->standard_coursemodule_elements();
 
         $this->add_action_buttons();
